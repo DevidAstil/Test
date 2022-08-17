31ef9b5aa727b2688bfcfcffa6062baaa10da57a
DevidAstil <111349164+DevidAstil@users.noreply.github.com>
2022-08-17 18:37:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
+$(document).ready(function(){
+    $('.reviews_slider').slick({
+        slidesToShow:3,
+        slidesToScroll:1,
+        centerMode:true,
+        variableWidth: true,
+        responsive:[
+            {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow:2,
+              centerMode:false,
+            },
+            breakpoint: 650,
+            settings: {
+              slidesToShow:1,
+            }
+            }
+        ]
+    })
 
+    var counter = new Date('aug 17 2022 16:30:00'); 
+    counter = new Date(counter); 
+    $('#m').countdown({until: counter}); 
+})
+
+function showText(){
+    $('.form-input-focus-1').css({
+        'opacity':'1'
+    })
+}
+function downText(){
+    $('.form-input-focus-1').css({
+        'opacity':'0'
+    })
+}
+function showText2(){
+    $('.form-input-focus-2').css({
+        'opacity':'1'
+    })
+}
+function downText2(){
+    $('.form-input-focus-2').css({
+        'opacity':'0'
+    })
+}
+$('.form-input-name').focus(showText)
+$('.form-input-name').blur(downText)
+$('.form-input-tel').focus(showText2)
+$('.form-input-tel').blur(downText2)
+    
